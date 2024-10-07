e2f70399a9871bdb2c9ede230d45bb5d3cb76743
abdulmalek <abdulmalek.esughi@ynov.com>
2024-10-07 20:12:32 +0200

    almost finshed challange 1

--- main.go
+++ main.go
@@ -8,18 +8,26 @@ import (
 )
 
 func main() {
-	temp, tempErr := template.ParseFiles("index.html")
+	temp, tempErr := template.ParseFiles("*.html")
 	if tempErr != nil {
 		fmt.Printf("erreur de templet %s", tempErr.Error())
 		os.Exit(02)
 	}
+
+	type User struct {
+		FristName string
+		LastName  string
+	}
 	type Ynov struct {
 		titre       string
-		filiere     string
-		niveau      string
-		nbrEtudiant int
+		Filiere     string
+		Niveau      string
+		NbrEtudiant int
+		Users       []User
 	}
-	http.HandleFunc("/promo", func(w http.ResponseWriter, r *http.Request) {
 
+	http.HandleFunc("/promo", func(w http.ResponseWriter, r *http.Request) {
+		data := Ynov{"307", "Cyber", "B1", 5, []User{{"Abdulmalek", "ESUGHI"}, {"Enzo", "ROSSI"}}}
+		temp.ExecuteTemplate(w, "index", data)
 	})
 }
