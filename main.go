90518298839c37bc87fcb1c638f21ad138564a50
abdulmalek <abdulmalek.esughi@ynov.com>
2024-10-07 11:41:49 +0200

    gestion d'erreur

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"html/template"
+	"os"
+)
 
+func main() {
+	temp, tempErr := template.ParseFiles("index.html")
+	if tempErr != nil {
+		fmt.Printf("erreur de templet %s", tempErr.Error())
+		os.Exit(02)
+	}
 }
