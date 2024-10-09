a699bf2c888ee961c794314c612bf538a78e00ea
abdulmalek <abdulmalek.esughi@ynov.com>
2024-10-09 12:00:12 +0200

    folder created

--- main.go
+++ main.go
@@ -22,14 +22,14 @@ type Ynov struct {
 }
 
 func main() {
-	temp, tempErr := template.ParseFiles("index.html")
+	temp, tempErr := template.ParseGlob("Templates/*.html")
 	if tempErr != nil {
-		fmt.Printf("erreur de templet %s", tempErr.Error())
+		fmt.Printf("Oups erreur avec le chargement du Template %s", tempErr.Error())
 		os.Exit(02)
 	}
 
 	http.HandleFunc("/promo", func(w http.ResponseWriter, r *http.Request) {
-		data := Ynov{"307", "Cyber", "B1", 5, []User{{"Abdulmalek", "ESUGHI", 20, "Masculin"}, {"Enzo", "ROSSI", 18, "Masculin"}}}
+		data := Ynov{"307", "Cyber", "B1", 2, []User{{"Abdulmalek", "ESUGHI", 20, "Masculin"}, {"Enzo", "ROSSI", 18, "Masculin"}}}
 		temp.ExecuteTemplate(w, "index", data)
 	})
 
